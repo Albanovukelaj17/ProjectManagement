1f3f150318cfbf5ca81f39cc439558c501eada3f
Albano <albigoal@outlook.com>
2024-09-06 13:48:40 +0200

    db neu

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/projects', projectsRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
